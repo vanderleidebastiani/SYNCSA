4db8e0e676364f9653ee9900ad1e64600dbc644b
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-12-03 15:36:23 -0800

    minor formatting

--- R/permut.row.matrix.R
+++ R/permut.row.matrix.R
@@ -3,14 +3,13 @@
 #' Internal function to permutate rows in a matrix.
 #' 
 #' 
-#' @author Vanderlei Júlio Debastiani <vanderleidebastiani@@yahoo.com.br>
+#' @author Vanderlei J?lio Debastiani <vanderleidebastiani@@yahoo.com.br>
 #' @keywords SYNCSA
-permut.row.matrix <-
-function(matrix){
+permut.row.matrix <- function(matrix){
 	row<-dim(matrix)[1]
 	col<-dim(matrix)[2]
 	samp<-sample(1:row,row)
 	permut.matrix <- matrix[samp,1:col]
 	return(permut.matrix)
-	}
+}
 
