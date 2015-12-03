a81bbdfb247e406d8c6f7964c9b84f00369bf8a5
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-12-03 15:02:38 -0800

    package documentation.

---
+++ R/SYNCSA-package.R
@@ -0,0 +1,19 @@
+
+
+#' SYNCSA
+#' 
+#' This is the syncsa package!
+#' 
+#' SYNCSA is a package that implements methods developed by Pillar, Duarte and
+#' others to measure functional trait convergence
+#' 
+#' @section Trait convergence
+#' 
+#' measured with TCAP
+#'   
+#' @docType package
+#' @name SYNCSA
+NULL
+
+
+
